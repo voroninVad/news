3dfe8afecc871df7b4f3cc022736341014db3bcd
Vadim Voronin <vadimvoronin831@gmail.com>
2024-09-21 19:34:40 +0300

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-/* eslint-disable no-undef */
-import { resolve as _resolve } from 'path';
+const path = require('path');
 
-export const entry = _resolve(__dirname, './src/index.ts');
-export const output = {
-    filename: 'bundle.js',
-    path: _resolve(__dirname, './dist'),
-    publicPath: '/',
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.js', '.ts'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    mode: 'development',
 };
-export const resolve = {
-    extensions: ['.js', '.ts'],
-};
-export const module = {
-    rules: [
-        {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        },
-    ],
-};
-export const mode = 'development';
